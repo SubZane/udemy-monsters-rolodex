3d0c410e44355622e928b09ba6e27d196cb33755
Andreas Lee-Norman <andreas.lee-norman@knowit.se>
2020-09-30 14:10:36 +0200

    Updates

---
+++ precache-manifest.26db9166f3ad2ad20a34a6b719c4df3d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b046a1cf2bc2e173d0a472ce3556715",
+    "url": "/udemy-monsters-rolodex/index.html"
+  },
+  {
+    "revision": "24cc535c418ce9b38e71",
+    "url": "/udemy-monsters-rolodex/static/css/main.8c36e813.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/udemy-monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/udemy-monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24cc535c418ce9b38e71",
+    "url": "/udemy-monsters-rolodex/static/js/main.4182a492.chunk.js"
+  },
+  {
+    "revision": "16a1d39489b9e2a4fb3c",
+    "url": "/udemy-monsters-rolodex/static/js/runtime-main.a1b71a5f.js"
+  }
+]);
